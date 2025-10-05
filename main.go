64aee0b3c00ef2e6c235454c5ae8847bbd440cfa
gulmix <gulmix@bk.ru>
2025-10-05 14:00:04 +0300

    ReadSystemMemory implemented

--- main.go
+++ main.go
@@ -94,6 +94,106 @@ func (d *DarwinMemoryReader) ReadProcessMemory(pid int) (uint64, error) {
 	return rssKb * 1024, nil
 }
 
+func (d *DarwinMemoryReader) ReadSystemMemory() (SystemMemoryInfo, error) {
+	cmd := exec.Command("sysctl", "-n", "hw.memsize")
+	output, err := cmd.Output()
+	if err != nil {
+		return SystemMemoryInfo{}, err
+	}
+	totalMemoryStr := strings.TrimSpace(string(output))
+	if totalMemoryStr == "" {
+		return SystemMemoryInfo{}, fmt.Errorf("Не удалось получить информации об общем объеме RAM")
+	}
+	totalMemory, err := strconv.ParseUint(totalMemoryStr, 10, 64)
+	if err != nil {
+		return SystemMemoryInfo{}, err
+	}
+	VmStats := make(map[string]uint64)
+	cmd = exec.Command("vm_stat")
+	output, err = cmd.Output()
+	if err != nil {
+		return SystemMemoryInfo{}, err
+	}
+	scanner := bufio.NewScanner(strings.NewReader(string(output)))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(line, "Mach Virtual Memory Statistics") {
+			continue
+		}
+		parts := strings.Split(line, ":")
+		if len(parts) != 2 {
+			continue
+		}
+		key := strings.TrimSpace(parts[0])
+		valueStr := strings.Trim(strings.TrimSpace(parts[1]), ".")
+
+		value, err := strconv.ParseUint(valueStr, 10, 64)
+		if err != nil {
+			continue
+		}
+		key = strings.TrimPrefix(key, "Pages ")
+		VmStats[key] = value
+	}
+	freePages := VmStats["free"] + VmStats["inactive"]
+	availablePages := VmStats["free"] + VmStats["inactive"] + VmStats["speculative"]
+	if fileCache, exists := VmStats["file-backed pages"]; exists {
+		availablePages += fileCache
+	} else if cache, exists := VmStats["cache"]; exists {
+		availablePages += cache
+	}
+	cmd = exec.Command("sysctl", "-n", "vm.swapusage")
+	output, err = cmd.Output()
+	if err != nil {
+		return SystemMemoryInfo{}, err
+	}
+	output = []byte(strings.TrimSpace(string(output)))
+	parts := strings.Fields(string(output))
+	if len(parts) < 9 {
+		return SystemMemoryInfo{}, fmt.Errorf("Неверный формат SwapInfo")
+	}
+	total, err := parseMemSize(parts[2])
+	if err != nil {
+		return SystemMemoryInfo{}, fmt.Errorf("Невозможно распарсить TotalSwap")
+	}
+	free, err := parseMemSize(parts[8])
+	if err != nil {
+		return SystemMemoryInfo{}, fmt.Errorf("Невозможно распарсить FreeSwap")
+	}
+	info := SystemMemoryInfo{
+		TotalMemory:     totalMemory,
+		FreeMemory:      freePages * uint64(4096),
+		AvailableMemory: availablePages * uint64(4096),
+		SwapTotal:       total,
+		SwapFree:        free,
+	}
+	return info, nil
+}
+
+func parseMemSize(sizeStr string) (uint64, error) {
+	var mult uint64 = 1
+	if strings.HasSuffix(sizeStr, "K") {
+		mult = 1024
+		sizeStr = strings.TrimSuffix(sizeStr, "K")
+	} else if strings.HasSuffix(sizeStr, "M") {
+		mult = 1024 * 1024
+		sizeStr = strings.TrimSuffix(sizeStr, "M")
+	} else if strings.HasSuffix(sizeStr, "G") {
+		mult = 1024 * 1024 * 1024
+		sizeStr = strings.TrimSuffix(sizeStr, "G")
+	} else if strings.HasSuffix(sizeStr, "T") {
+		mult = 1024 * 1024 * 1024 * 1024
+		sizeStr = strings.TrimSuffix(sizeStr, "T")
+	}
+	val, err := strconv.ParseFloat(sizeStr, 64)
+	if err != nil {
+		return 0, err
+	}
+	return uint64(val * float64(mult)), nil
+}
+
 func (l *LinuxMemoryReader) GetProcessList() ([]int, error) {
 	var pids []int
 
